46339ea73925c795641d05b3e50b13c6c35a409a
jessegreatowoicho-ui <jessegreatowoicho@gmail.com>
2026-07-17 17:52:21 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,12 @@
+{
+  "name": "pokemon-casino-bot",
+  "version": "1.0.0",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.5",
+    "qrcode-terminal": "^0.12.0"
+  }
+}
