c0c160c17b41f1d6917d218f197b3f5e175de4cd
jessegreatowoicho-ui <jessegreatowoicho@gmail.com>
2026-07-18 13:22:57 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    START_COINS: 1000,
+
+    DAILY_REWARD: 500,
+
+    PACK_PRICE: 100,
+
+    BATTLE_REWARD: 500,
+
+    MAX_HAND: 7,
+
+    DECK_SIZE: 20,
+
+    PREFIX: "!",
+
+    BOT_NAME: "BOT BOY"
+
+};
